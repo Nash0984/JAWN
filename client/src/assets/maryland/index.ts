e16ac189a44cf342720af0ccc3bf53fd4238eeb6
Nash0984 <graham.oneill@gmail.com>
2025-12-25 00:48:28 +0000

    Mnf

--- client/src/assets/maryland/index.ts
+++
@@ -1,9 +0,0 @@
-import sealSvg from './seal.svg';
-import flagSvg from './flag.svg';
-
-export const MarylandAssets = {
-  seal: sealSvg,
-  flag: flagSvg,
-} as const;
-
-export { sealSvg, flagSvg };
